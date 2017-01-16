10d184f1db64e3c43d910bebea6fea3bbf5b9512
hiroyuki arie <user@ariehiroyuki-6.local>
2017-01-16 20:05:22 +0900

    first_commit

---
+++ dev/decompiler/common.js
@@ -0,0 +1,19 @@
+$(function(){
+
+	// hsize = $(window).height();
+	// $('.inner').css('height', hsize + 'px');
+	//
+	// $(window).resize(function() {
+	// 	hsize = $(window).height();
+	// 	$('.inner').css('height', hsize + 'px');
+	// });
+	//
+	// hsize = $(window).height();
+	// $('.text-sec').css('height', hsize + 'px');
+	//
+	// $(window).resize(function() {
+	// 	hsize = $(window).height();
+	// 	$('.text-sec').css('height', hsize + 'px');
+	// });
+
+});
