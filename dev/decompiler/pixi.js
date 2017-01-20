358ac263fd9b630ea07e32c9c725c8a65b4123b4
hiroyuki arie <user@ariehiroyuki-6.local>
2017-01-20 18:22:43 +0900

    pixi手書きで書いてみた

--- dev/decompiler/pixi.js
+++ dev/decompiler/pixi.js
@@ -9,34 +9,27 @@ $(function(){
 	});
 
   var width = 600;
-  var height = 600;
-
-  // ステージを作る
+  var height = 400;
   var stage = new PIXI.Stage(0x000000);
+  var renderer = PIXI.autoDetectRenderer(width,height);
 
-  // レンダラーを作る
-  var renderer = PIXI.autoDetectRenderer(width, height);
-
-  // レンダラーのviewをDOMに追加する
   document.getElementById("pixiview").appendChild(renderer.view);
 
-  // テキストオブジェクトを作る
   var word = "Hello World!";
-  var style = {font:'bold 20pt Arial', fill:'white'};
+  var style = {font:"bold 60pt Futura-PT", fill:"white"};
   var textobj = new PIXI.Text(word, style);
-  textobj.position.x = width / 2;
+  textobj.position.x = 60;
   textobj.position.y = height / 2;
 
-  // テキストオブジェクトをステージに乗せる
   stage.addChild(textobj);
 
-  // アニメーション関数を定義する
   function animate(){
-      // requestAnimationFrame(animate); // 次の描画タイミングでanimateを呼び出す
-      // textobj.rotation += 0.01; // テキストを回転する
-      renderer.render(stage);   // 描画する
+    requestAnimationFrame(animate);
+    textobj.rotation += 0.01;
+    renderer.render(stage);
   }
 
-  // 次のアニメーションフレームでanimate()を呼び出してもらう
-  animate();
+  requestAnimationFrame(animate);
+
+
 });
