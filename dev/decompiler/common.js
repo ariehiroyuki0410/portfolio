6abf224998e70d4beb027eeb9183e5349db02b78
hiroyuki arie <arie@pnrm.co.jp>
2017-02-21 19:39:39 +0900

    大方完成

--- dev/decompiler/common.js
+++ dev/decompiler/common.js
@@ -1,19 +1,5 @@
-$(function(){
-
-	// hsize = $(window).height();
-	// $('.inner').css('height', hsize + 'px');
-	//
-	// $(window).resize(function() {
-	// 	hsize = $(window).height();
-	// 	$('.inner').css('height', hsize + 'px');
-	// });
-	//
-	// hsize = $(window).height();
-	// $('.text-sec').css('height', hsize + 'px');
-	//
-	// $(window).resize(function() {
-	// 	hsize = $(window).height();
-	// 	$('.text-sec').css('height', hsize + 'px');
-	// });
-
+$(window).load(function () {
+  var filterizd = $('.filtr-container').filterizr({
+   //options object
+  });
 });
